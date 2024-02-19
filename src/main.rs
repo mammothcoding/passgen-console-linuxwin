7f7eeb2742e7691c32a8404452783b077425ebbb
mammoth <mammmoth@list.ru>
2024-02-20 02:56:48 +0300

    pre 1

--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,174 @@
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use std::{error::Error, io};
-use tui::{
-    backend::{Backend, CrosstermBackend},
-    layout::{Alignment, Constraint, Direction, Layout},
-    style::{Color, Modifier, Style},
-    text::Span,
-    widgets::{Block, BorderType, Borders},
-    Frame, Terminal,
-};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
 
-    // create app and run it
-    let res = run_app(&mut terminal);
+fn main() {
+    use std::io::{self, BufRead};
+    use std::process;
+    use rand::Rng;
+    const PASSWORD_LEN: usize = 30;
+    const LETTERS_CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz";
+    const NUMBERS_CHARSET: &[u8] = b"0123456789";
+    const SPEC_SYMB_CHARSET: &[u8] = b")([]{}*&^%$#@!~";
+    const LET_NUM_DRC_FREE: &[u8] = b"ABCDEFGHJKMNPQRSTUVWXYZ\
+                            abcdefghjkmnpqrstuvwxyz\
+                            23456789";
 
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
 
-    if let Err(err) = res {
-        println!("{:?}", err)
+    pub struct Rules {
+        letters: bool,
+        numbs: bool,
+        spec_symbs: bool,
+        let_num_drc_free: bool,
     }
 
-    Ok(())
-}
+    let start_rules = Rules {
+        letters: true,
+        numbs: true,
+        spec_symbs: true,
+        let_num_drc_free: true,
+    };
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
-    loop {
-        terminal.draw(ui)?;
+    impl Rules {
+        fn init(self) -> Rules {
+            let letters;
+            let numbs;
+            let spec_symbs;
+            let let_num_drc_free;
 
-        if let Event::Key(key) = event::read()? {
-            if let KeyCode::Char('q') = key.code {
-                return Ok(());
+            println!("Hello, mother fucker!");
+            println!("Letters?");
+            let input = io::stdin()
+                .lock()
+                .lines()
+                .next()
+                .unwrap()
+                .unwrap()
+                .parse::<String>();
+            match input {
+                Ok(ans) =>
+                    match ans.as_str() {
+                        "y" => letters = true,
+                        "n" => letters = false,
+                        _ => {
+                            println!("Illegal input! Bye!");
+                            process::exit(1);
+                        },
+                    }
+                Err(_) => {
+                    process::exit(1);
+                }
+            }
+
+            println!("Numbers?");
+            let input = io::stdin()
+                .lock()
+                .lines()
+                .next()
+                .unwrap()
+                .unwrap()
+                .parse::<String>();
+            match input {
+                Ok(ans) =>
+                    match ans.as_str() {
+                        "y" => numbs = true,
+                        "n" => numbs = false,
+                        _ => {
+                            println!("Illegal input! Bye!");
+                            process::exit(1);
+                        },
+                    }
+                Err(_) => {
+                    process::exit(1);
+                }
+            }
+
+            println!("Special symbols?");
+            let input = io::stdin()
+                .lock()
+                .lines()
+                .next()
+                .unwrap()
+                .unwrap()
+                .parse::<String>();
+            match input {
+                Ok(ans) =>
+                    match ans.as_str() {
+                        "y" => spec_symbs = true,
+                        "n" => spec_symbs = false,
+                        _ => {
+                            println!("Illegal input! Bye!");
+                            process::exit(1);
+                        },
+                    }
+                Err(_) => {
+                    process::exit(1);
+                }
+            }
+
+            println!("Letters and numbers without doubly readable characters?");
+            let input = io::stdin()
+                .lock()
+                .lines()
+                .next()
+                .unwrap()
+                .unwrap()
+                .parse::<String>();
+            match input {
+                Ok(ans) =>
+                    match ans.as_str() {
+                        "y" => let_num_drc_free = true,
+                        "n" => let_num_drc_free = false,
+                        _ => {
+                            println!("Illegal input! Bye!");
+                            process::exit(1);
+                        },
+                    }
+                Err(_) => {
+                    process::exit(1);
+                }
+            }
+
+            Rules {
+                letters,
+                numbs,
+                spec_symbs,
+                let_num_drc_free,
             }
         }
+
+        fn generate_pass(self) {
+            let mut rng = rand::thread_rng();
+            let mut pass_charset: & [u8] = b"";
+            //let mut pass_charset: & [u8] = LET_NUM_DRC_FREE;
+
+            /*if self.let_num_drc_free == true {
+                pass_charset = &*[pass_charset, LET_NUM_DRC_FREE].concat();
+            } else {
+                if self.letters == true {
+                    pass_charset = &*[pass_charset, LETTERS_CHARSET].concat();
+                }
+                if self.numbs == true {
+                    pass_charset = &*[pass_charset, NUMBERS_CHARSET].concat();
+                }
+            }
+            if self.spec_symbs == true {
+                pass_charset = &*[pass_charset, SPEC_SYMB_CHARSET].concat();
+            }*/
+            if pass_charset.len() == 0 {
+                pass_charset = &*[pass_charset, LET_NUM_DRC_FREE].concat();
+            }
+
+            let password: String = (0..PASSWORD_LEN)
+                .map(|_| {
+                    let idx = rng.gen_range(0..pass_charset.len());
+                    pass_charset[idx] as char
+                })
+                .collect();
+
+            println!("Password is:");
+            println!("{:?}", password);
+        }
     }
-}
 
-fn ui<B: Backend>(f: &mut Frame<B>) {
-    // Wrapping block for a group
-    // Just draw the block and the group on the same area and build the group
-    // with at least a margin of 1
-    let size = f.size();
-
-    // Surrounding block
-    let block = Block::default()
-        .borders(Borders::ALL)
-        .title("Main block with round corners")
-        .title_alignment(Alignment::Center)
-        .border_type(BorderType::Rounded);
-    f.render_widget(block, size);
-
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(4)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(f.size());
-
-    // Top two inner blocks
-    let top_chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(chunks[0]);
-
-    // Top left inner block with green background
-    let block = Block::default()
-        .title(vec![
-            Span::styled("With", Style::default().fg(Color::Yellow)),
-            Span::from(" background"),
-        ])
-        .style(Style::default().bg(Color::Green));
-    f.render_widget(block, top_chunks[0]);
-
-    // Top right inner block with styled title aligned to the right
-    let block = Block::default()
-        .title(Span::styled(
-            "Styled title",
-            Style::default()
-                .fg(Color::White)
-                .bg(Color::Red)
-                .add_modifier(Modifier::BOLD),
-        ))
-        .title_alignment(Alignment::Right);
-    f.render_widget(block, top_chunks[1]);
-
-    // Bottom two inner blocks
-    let bottom_chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(chunks[1]);
-
-    // Bottom left block with all default borders
-    let block = Block::default().title("With borders").borders(Borders::ALL);
-    f.render_widget(block, bottom_chunks[0]);
-
-    // Bottom right block with styled left and right border
-    let block = Block::default()
-        .title("With styled borders and doubled borders")
-        .border_style(Style::default().fg(Color::Cyan))
-        .borders(Borders::LEFT | Borders::RIGHT)
-        .border_type(BorderType::Double);
-    f.render_widget(block, bottom_chunks[1]);
+    start_rules.init().generate_pass();
 }
